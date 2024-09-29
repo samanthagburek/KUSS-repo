315dbc931fc615daa730b35da1189d0fff8bfdc1
Uma Nachiappan <uma.nachi@gmail.com>
2024-09-28 22:39:14 -0400

    uma's new endpoint

--- server/endpoints.py
+++ server/endpoints.py
@@ -18,6 +18,9 @@ ENDPOINT_EP = '/endpoints'
 ENDPOINT_RESP = 'Available endpoints'
 HELLO_EP = '/hello'
 HELLO_RESP = 'hello'
+TITLE_EP = '/title'
+TITLE_RESP = 'Title: '
+TITLE = 'The Journal of the KUSS Project'
 
 
 @api.route(HELLO_EP)
@@ -34,7 +37,7 @@ class HelloWorld(Resource):
         return {HELLO_RESP: 'world'}
 
 
-@api.route('/endpoints')
+@api.route(ENDPOINT_EP)
 class Endpoints(Resource):
     """
     This class will serve as live, fetchable documentation of what endpoints
@@ -46,3 +49,16 @@ class Endpoints(Resource):
         """
         endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
         return {"Available endpoints": endpoints}
+
+
+@api.route(TITLE_EP)
+class JournalTitle(Resource):
+    """
+    This class handles creating, reading, updating,
+    and deleting the journal title.
+    """
+    def get(self):
+        """
+        Retrieves the journal title.
+        """
+        return {TITLE_RESP: TITLE}
