472d8317eba581580775cd2b6d98740f8fd79fee
Uma Nachiappan <uma.nachi@gmail.com>
2024-10-16 10:10:35 -0400

    fixing new create-person endpoint

--- server/endpoints.py
+++ server/endpoints.py
@@ -4,7 +4,7 @@ The endpoint called `endpoints` will return all available endpoints.
 """
 from http import HTTPStatus
 
-from flask import Flask  # request
+from flask import Flask, request
 from flask_restx import Resource, Api, fields  # Namespace
 from flask_cors import CORS
 
