a9f4c54514e494d7731c5f02f8cb82103f69d0eb
Uma Nachiappan <uma.nachi@gmail.com>
2024-10-16 10:05:41 -0400

    testing read() for text.py

--- data/tests/test_text.py
+++ data/tests/test_text.py
@@ -2,3 +2,11 @@ import pytest
 
 import data.text as txt
 
+
+def test_read():
+    text = txt.read()
+    assert isinstance(text, dict)
+    assert len(text) > 0
+    for _id, thing in text.items():
+        assert isinstance(_id, str)
+        assert txt.TITLE in thing
