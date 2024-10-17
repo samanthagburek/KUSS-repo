bd7cd5363ee899c5e1b7b0b74d5a569c0ed962cf
Farhen Shefa <fas6488@nyu.edu>
2024-10-17 12:27:53 -0400

    added test_delete_people

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -77,9 +77,29 @@ def test_create_people():
    assert "return" in resp_json, "Expected 'return' in response"
    assert resp_json["return"] == person_data[EMAIL], f"Expected return to be {person_data[EMAIL]}, but got {resp_json['return']}"
 
-#    for _id, person in resp_json.items():
-#        assert isinstance(_id, str)
-#        assert len(_id) > 0
-#        assert NAME in person
-#        assert AFFILIATION in person
-#        assert EMAIL in person
+
+def test_delete_people():
+   # creating a person to make sure the person we are trying to delete, exists
+   person_data ={ 
+        NAME: "Jane Street",
+        AFFILIATION: "YOU",
+        EMAIL: "jane@you.org"
+    }
+    
+   create_resp = TEST_CLIENT.put(f'{ep.PEOPLE_EP}/create', json=person_data)
+   create_resp_json = create_resp.get_json()
+   created_email = create_resp_json.get("return")
+ 
+   assert created_email == person_data[EMAIL], f"Expected return to be {person_data[EMAIL]}, but got {resp_json['return']}"
+
+   del_resp = TEST_CLIENT.delete(f'{ep.PEOPLE_EP}/{created_email}')
+   del_resp_json = delete_resp.get_json()
+
+   assert "Deleted" in del_resp_json, "Expected 'Deleted' in response"
+   assert del_resp_json["Deleted"] == created_email, f"Expected deleted ID to be {created_email} but got {del_resp_json['Deleted']}"
+
+   # verifying if deleted
+   read_resp = TEST_CLIENT.get(ep.PEOPLE_EP)
+   read_resp_json = read_resp.get_json()
+   
+   assert created_email not in read_resp_json, "Person still found after delete"
