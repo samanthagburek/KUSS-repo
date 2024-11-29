805ad011a5caa6489b9e8c124e1a13c1f0161fa8
samanthagburek <sgg2056@nyu.edu>
2024-11-28 23:17:13 -0500

    query.py add actions

--- data/manuscripts/query.py
+++ data/manuscripts/query.py
@@ -14,3 +14,21 @@ def get_states() -> list:
     return VALID_STATES
 def is_valid_state(state: str) -> bool:
     return state in VALID_STATES
+
+# actions:
+ACCEPT = 'ACC'
+ASSIGN_REF = 'ARF'
+DONE = 'DON'
+REJECT = 'REJ'
+# for testing:
+TEST_ACTION = ACCEPT
+VALID_ACTIONS = [
+    ACCEPT,
+    ASSIGN_REF,
+    DONE,
+    REJECT,
+]
+def get_actions() -> list:
+    return VALID_ACTIONS
+def is_valid_action(action: str) -> bool:
+    return action in VALID_ACTIONS
