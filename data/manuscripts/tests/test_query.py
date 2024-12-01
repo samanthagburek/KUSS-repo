9b66a3bffc5365329c5ab8206056860cc8a3257c
Rian Shinozaki <redkrsc@gmail.com>
2024-12-01 13:44:23 -0500

    Added tests for handling actions

--- data/manuscripts/tests/test_query.py
+++ data/manuscripts/tests/test_query.py
@@ -21,3 +21,14 @@ def test_is_not_valid_action():
     # run this test "a few" times
     for i in range(10):
         assert not mqry.is_valid_action(gen_random_not_valid_str())
+
+def handle_action_bad_state():
+    with pytest.raises(ValueError):
+        mqry.handle_action(gen_random_not_valid_str(),
+                           mqry.TEST_ACTION)
+
+def test_handle_action_valid_return():
+    for state in mqry.get_states():
+        for action in mqry.get_actions():
+            new_state = mqry.handle_action(state, action)
+            assert mqry.is_valid_state(new_state)
