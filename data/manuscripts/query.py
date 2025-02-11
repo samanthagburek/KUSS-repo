11276fcf249497e827423b9ecea301a25b6359ae
samanthagburek <sgg2056@nyu.edu>
2025-02-11 11:03:29 -0500

    small linter fixes

--- data/manuscripts/query.py
+++ data/manuscripts/query.py
@@ -9,7 +9,7 @@ SUBMITTED = 'SUB'
 FORMATTING = 'FOR'
 PUBLISHED = 'PUB'
 WITHDRAWN = 'WIT'
-IN_AUTH_REVISION = 'IAR' # author revisions
+IN_AUTH_REVISION = 'IAR'  # author revisions
 
 TEST_STATE = SUBMITTED
 VALID_STATES = [
@@ -31,12 +31,14 @@ SAMPLE_MANU = {
     flds.REFEREES: [],
 }
 
+
 def get_states() -> list:
     return VALID_STATES
 
 
 def is_valid_state(state: str) -> bool:
     return state in VALID_STATES
+
 
 # actions:
 ACCEPT = 'ACC'
@@ -60,15 +62,19 @@ VALID_ACTIONS = [
     SUBMIT_REV
 ]
 
+
 def get_actions() -> list:
     return VALID_ACTIONS
+
 
 def is_valid_action(action: str) -> bool:
     return action in VALID_ACTIONS
 
+
 def assign_ref(manu: dict, ref: str, extra=None) -> str:
     manu[flds.REFEREES].append(ref)
     return IN_REF_REV
+
 
 def delete_ref(manu: dict, ref: str) -> str:
     if len(manu[flds.REFEREES]) > 0:
@@ -77,6 +83,7 @@ def delete_ref(manu: dict, ref: str) -> str:
         return IN_REF_REV
     else:
         return SUBMITTED
+
 
 FUNC = 'f'
 
@@ -127,13 +134,13 @@ STATE_TABLE = {
         DONE: {
             FUNC: lambda **kwargs: EDITOR_REV,
         },
-         **COMMON_ACTIONS,
+        **COMMON_ACTIONS,
     },
-    EDITOR_REV:{
+    EDITOR_REV: {
         ACCEPT: {
             FUNC: lambda **kwargs: COPY_EDIT,
         },
-         **COMMON_ACTIONS,
+        **COMMON_ACTIONS,
     },
     AUTHOR_REV: {
         DONE: {
@@ -144,7 +151,7 @@ STATE_TABLE = {
     REJECTED: {
          **COMMON_ACTIONS,
     },
-    WITHDRAWN:{
+    WITHDRAWN: {
         **COMMON_ACTIONS,
     },
     PUBLISHED: {
@@ -158,8 +165,10 @@ STATE_TABLE = {
     },
 }
 
+
 def get_valid_actions_by_state(state: str) -> list:
     return STATE_TABLE[state].keys()
+
 
 def handle_action(current_state: str, action: str, **kwargs) -> str:
     if current_state not in STATE_TABLE:
@@ -167,6 +176,7 @@ def handle_action(current_state: str, action: str, **kwargs) -> str:
     if action not in STATE_TABLE[current_state]:
         raise ValueError(f'{action} not available in {current_state}')
     return STATE_TABLE[current_state][action][FUNC](**kwargs)
+
 
 def main():
     print(handle_action(SUBMITTED, ASSIGN_REF,
@@ -178,14 +188,15 @@ def main():
     # put this back in to handle no ref -> submitted state
     # print(handle_action(IN_REF_REV, DELETE_REF, manu=SAMPLE_MANU,
     #                     ref='Jack'))
-    #print(handle_action(SUBMITTED, WITHDRAW, manu=SAMPLE_MANU))
-    #print(handle_action(SUBMITTED, REJECT, manu=SAMPLE_MANU))
+    # print(handle_action(SUBMITTED, WITHDRAW, manu=SAMPLE_MANU))
+    # print(handle_action(SUBMITTED, REJECT, manu=SAMPLE_MANU))
     print(handle_action(IN_REF_REV, ACCEPT_REV, manu=SAMPLE_MANU))
     print(handle_action(IN_AUTH_REVISION, DONE, manu=SAMPLE_MANU))
-    print(handle_action(IN_EDIT_REV, ACCEPT, manu=SAMPLE_MANU))
+    print(handle_action(EDITOR_REV, ACCEPT, manu=SAMPLE_MANU))
     print(handle_action(COPY_EDIT, DONE, manu=SAMPLE_MANU))
     print(handle_action(AUTHOR_REV, DONE, manu=SAMPLE_MANU))
     print(handle_action(FORMATTING, DONE, manu=SAMPLE_MANU))
+
 
 if __name__ == '__main__':
     main()
